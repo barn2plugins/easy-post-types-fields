6cd163abb29557896e2e21d00e975ab18aa637c6
Luigi Pulcini <luigi@barn2.com>
2022-04-26 11:24:14 +0200

    changed Setup Wizard Welcome step description

--- src/Admin/Wizard/Steps/Welcome.php
+++ src/Admin/Wizard/Steps/Welcome.php
@@ -16,8 +16,7 @@ class Welcome extends Welcome_Free {
 		$this->set_id( 'welcome' );
 		$this->set_name( esc_html__( 'Welcome', 'easy-post-types-fields' ) );
 		$this->set_title( esc_html__( 'Welcome to Easy Post Types and Fields', 'easy-post-types-fields' ) );
-		$this->set_description( esc_html__( 'Create custom content types and data in no time', 'easy-post-types-fields' ) );
-		$this->set_tooltip( esc_html__( 'Use this setup wizard to quickly create a custom post type. Your post type will have its own section in the WordPress admin, and can be managed separately from the other content on your site. For example, you might create a custom post type for Events, Members, Articles, or something else.', 'easy-post-types-fields' ) );
+		$this->set_description( esc_html__( 'Use this setup wizard to quickly create a custom post type (e.g. Events, Members, Articles, or something else). Your post type will have its own section in the WordPress admin. If you want to add fields to existing post types, then skip the setup wizard and go to the \'Other post types\' section.', 'easy-post-types-fields' ) );
 	}
 
 }
